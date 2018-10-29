0db7efd3246aa35563ff6b39f29cd24d6be82be5
Aaron Rosenberg <aaronrbg@gmail.com>
2018-10-29 11:29:59 -0700

    init with readme and avatar.js

---
+++ avatar.js
@@ -0,0 +1,11 @@
+//INPUT
+//github user, repository name
+
+//OUTPUT:
+//make folder called avatars
+//fill with images of repo contributors where each image is according to contributor's name
+
+function download_avatars() {
+
+}
+
