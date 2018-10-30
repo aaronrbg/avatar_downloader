32c914125bf57d721205fe11c29ae6f8490e212c
Aaron Rosenberg <aaronrbg@gmail.com>
2018-10-30 01:13:12 +0000

    got the program to run without errors

--- avatar.js
+++ avatar.js
@@ -1,13 +1,32 @@
 var request = require('request');
 var dotenv = require('dotenv').config();
+var fs = require('fs');
 
 var github_token = process.env.GITHUB_TOKEN
 
-function getRepoContributors(repoOwner, repoName, cb) {
-  // ...
-}
+//INPUT: github username and one of thier reposiotry names
+//OUTPUT: an array of objects with the name and url of repo contributor images
+function getRepoContributors(repoOwner, repoName, callback) {
+
+  // var options = {
+    var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors/?access_token=" + github_token
+  //   headers: {
+  //     'User-Agent': 'request'
+  //   }
+  // };
+
+  request.get(url)
+    .on('error', function (err) {                                   
+      throw err; 
+    })
+    .on('response', function (response) {                           
+      console.log('IM HERE')
+    })
 
 function downloadImageByURL(url, filePath) {
   // ...
 }
 
+}
+
+getRepoContributors('jquery', 'jquery')
